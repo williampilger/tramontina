0da2f6bdb00cca8df3276eb7edf3ac66c7bbba87
William Pilger <pilger.will@gmail.com>
2021-03-09 16:13:16 -0300

    Update
    Adicionada a função de comras. E finalmente operante.

--- Requisição de materiais/Python/Requisicao.py
+++ Requisição de materiais/Python/Requisicao.py
@@ -1,28 +1,101 @@
 import pyautogui
+import time
 
 def requisita_materiais_simples():
+    print("ATENÇÃO: Garanta que o CMP072 seja a proxima janela do seu ALT+TAB e esteja com o cursor posicionado no campo ITEM !")
+    secao = input("Digite a seção de entrega: ")
+    pyautogui.hotkey('alt', 'tab')
+    cont_erp = 0 #vaiavel conta os dígitos do ERP
     with open("materiais.txt", "r") as arquivo:
         for linha in arquivo:
             etapa = 0
             for letra in linha:
-                if(letra == '\n'):
-                    break
-                if(letra == '\t'):
-                    if (etapa == 0):#acabou de digital ERP
-                        press('\t')
-
+                if(letra == '\n' and etapa == 0):
+                    print("Final de linha encontrado antes do esperado")
+                    return
+                if(letra == '\t' or letra == '\n'):
+                    if (etapa == 0):#acabou de digitar ERP
+                        if(cont_erp != 6):
+                            print("ERP informado não tem 6 digitos")
+                            return
+                        pyautogui.press('tab')
+                        cont_erp = 0
+                    elif(etapa == 1):#acabou de digitar a quantidade
+                        pyautogui.press('tab')
+                        pyautogui.press('s')
+                        pyautogui.press('tab')
+                        pyautogui.typewrite(secao)
+                        pyautogui.press('tab')
                     etapa += 1
                 else:
                     if(etapa == 0):#obtendo ERP
-                        press(letra)
+                        pyautogui.press(letra)
+                        cont_erp += 1
                     if(etapa == 1):#obtendo quantidade
-                        press(letra)
+                        pyautogui.press(letra)
                 if(etapa == 2):
-                    break
-
-
+                    break #ignora o resto da linha
+            time.sleep(1)#1s entre uma requisição e outra
+            
 def solicita_compra():
-    pass
+    print("ATENÇÃO: Garanta que o CMP076 seja a proxima janela do seu ALT+TAB e esteja com o cursor posicionado no campo ITEM !")
+    input("Tecle ENTER quando estiver pronto: ")
+    pyautogui.hotkey('alt', 'tab')
+    cont_erp = 0 #variavel conta os dígitos do ERP
+    with open("materiais.txt", "r") as arquivo:
+        for linha in arquivo:
+            etapa = 0
+            for letra in linha:
+                if(letra == '\n' and etapa <= 2):
+                    print("Final de linha encontrado antes do esperado")
+                    return
+                if(letra == '\t' or letra == '\n'):
+                    if (etapa == 0):#acabou de digitar ERP
+                        if(cont_erp != 6):
+                            print("ERP informado não tem 6 digitos")
+                            return
+                        pyautogui.press('tab')
+                        time.sleep(0.1)
+                        pyautogui.hotkey('ctrl', '1')
+                        time.sleep(0.1)
+                        pyautogui.press('enter')
+                        time.sleep(0.1)
+                        cont_erp = 0
+                    elif(etapa == 1):#acabou de digitar a quantidade
+                        pyautogui.press('tab') #confirma quantidade
+                        time.sleep(0.1)
+                        pyautogui.press('s') #entra para preencher obs tecnica
+                        time.sleep(0.1)
+                        pyautogui.press('tab') #entra no campo obs interna
+                        time.sleep(0.1)
+                    #OPCAO == 2 acaba de varrer a descrição do item
+                    elif(etapa == 3):
+                        pyautogui.press('enter')#confirma OBS
+                        time.sleep(0.1)
+                        pyautogui.press('enter')#entra na edição da data
+                        time.sleep(0.1)
+                        pyautogui.press('enter')#preenche data de hoje
+                        time.sleep(0.1)
+                        pyautogui.press('enter')#confirma quantidade todal
+                        time.sleep(0.1)
+                        pyautogui.hotkey('ctrl', 'enter')#confirma data e quantidade de entrega
+                        time.sleep(0.1)
+                        pyautogui.press('enter')#finaliza requisição
+                        time.sleep(0.1)
+                    etapa += 1
+                else:
+                    if(etapa == 0):#obtendo ERP
+                        pyautogui.press(letra)
+                        cont_erp += 1
+                    elif(etapa == 1):#obtendo quantidade
+                        pyautogui.press(letra)
+                    #etapa == 2 varrendo descrição do ítem
+                    elif(etapa == 3):#digitando obs interna
+                        pyautogui.press(letra)
+                if(etapa == 4):
+                    break #ignora o resto da linha, caso chegue aqui
+            time.sleep(1)#1s entre uma requisição e outra
+
 
 def mensagem_sobre():
     print("Versão V0.0")
@@ -45,32 +118,3 @@ def inicio():
 if (__name__ == "__main__"):
     inicio()
     print("FIM DO PROGRAMA!")
-
-"""
-def carrega_arquivo():
-    codigos = []
-    quantidades = []
-    temp_erp = [] #armazena os digitos do erp individualmente durante o processamento da linha
-    temp_quant = []  #armazena os digitos da quantidade individualmente durante o processamento da linha
-    with open("materiais.txt", "r") as arquivo:
-        for linha in arquivo:
-            etapa = 0
-            for letra in linha:
-                if(letra == '\n'):
-                    break
-                if(letra == '\t'):
-                    etapa += 1
-                else:
-                    if(etapa == 0):#obtendo ERP
-                        temp_erp.append(letra)
-                    if(etapa == 1):#obtendo quantidade
-                        temp_quant.append(letra)
-                if(etapa == 2):
-                    break
-            #processa os dados obtidos de uma linha
-            print(temp_erp)
-            print(temp_quant)
-
-            temp_quant.clear()
-            temp_erp.clear()
-"""
