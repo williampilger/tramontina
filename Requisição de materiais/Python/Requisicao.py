c965cc455b9ad65268b1dee60fad1753462496da
William Pilger <pilger.will@gmail.com>
2021-03-12 12:56:52 -0300

    Update

--- Requisição de materiais/Python/Requisicao.py
+++ Requisição de materiais/Python/Requisicao.py
@@ -196,7 +196,7 @@ def solicita_compra_avancado():
 
 
 def mensagem_sobre():
-    print("Versão V1.1")
+    print("Versão V1.2")
     print("\n Desenvolvido por William Pilger")
     print("\n\n Historico de alterações:")
     print(" 10/03/2021 - Delays dinâminos na parte de compra de materiais;")
