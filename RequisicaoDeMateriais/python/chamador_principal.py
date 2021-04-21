5fc30f00972e4b947a9fb54b918ded9ea059e722
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-04-21 12:53:35 -0300

    Update

--- RequisicaoDeMateriais/python/chamador_principal.py
+++ RequisicaoDeMateriais/python/chamador_principal.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+
 def restart_program():
     os.system(f"python \"{__file__}\"")
     quit()
@@ -9,17 +10,19 @@ except:
     print("\nINSTALANDO BIBLIOTECA NECESSÁRIA, AGUARDE!\n(CONEXÃO COM INTERNET NECESSÁRIA)")
     os.system("pip install requests")
     restart_program()
-def baixa_arquivo_temp(url):
+
+def baixa_arquivo(url, destino):
     dados = requests.get(url)#faz download do arquivo
-    temparquivo = tempfile.TemporaryFile()
-    temparquivo.write(dados.content)
-    temparquivo.close()
-    return temparquivo.name
-    os.system(f"python {temparquivo.name}")
+    if (dados.status_code != requests.codes.OK):
+        print("Falha ao obter o arquivo fonte. Tentaremos instalar os sertificados, e na sequência poderá tentar novamente.")
+        os.system("pip install python-certifi-win32") #já que não sei se está instalada, instalo sempre que falhar
+        restart_program()
+    with open(destino, "wb") as arquivo:
+        arquivo.write(dados.content)
 
 def main():
-    programa = baixa_arquivo_temp("")
-    os.system(f"python {programa}")
+    programa = baixa_arquivo("https://raw.githubusercontent.com/williampilger/tramontina/master/RequisicaoDeMateriais/python/cpm_autotype.py", "cache")
+    os.system(f"python cache")
     quit()
 
 if __name__ == "__main__":
